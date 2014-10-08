e0c1fa5eb09dde6f35c53558c7c887ac5b7d4f99
cjr <dolt131943@users.noreply.github.com>
2014-10-08 11:55:18 +0800

    pc3

--- src/pc2-s2.py
+++ src/pc2-s2.py
@@ -1,5 +1,10 @@
-import string
+import string,sys
+
 encode_str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+if (len(sys.argv) >1 and sys.argv[1] != ""):
+    print "args[1]=" + sys.argv[1]
+    encode_str = sys.argv[1]
 
 
 from_chrs = "abcdefghijklmnopqrstuvwxyz"
