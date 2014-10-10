5cf0f273ec3f6dc15626306529537e76b520ba52
cjr <dolt131943@users.noreply.github.com>
2014-10-10 17:52:18 +0800

    pc12

---
+++ src/pc12.py
@@ -0,0 +1,11 @@
+import Image,urllib,StringIO
+
+im = Image.open(StringIO.StringIO(urllib.urlopen('http://www.pythonchallenge.com/pc/return/cave.jpg').read()))
+w,h = im.size
+
+for i in range(w):
+    for j in range(h):
+        if ((i+j) % 2) == 1:
+            im.putpixel((i,j),0)
+
+im.save('cave_new.png')
